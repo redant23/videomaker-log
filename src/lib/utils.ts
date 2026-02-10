5ac2eee68c851edbe7bc31da0a2579c33ac63f20
redant <kimdongjun23@gmail.com>
2026-02-10 15:19:42 +0900

    redeploy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,7 +11,7 @@ export function getURL() {
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Vercel 배포 주소
     'http://localhost:3000/'; // 로컬 주소
 
-  // 끝에 슬래시 포함 여부 확인 및 http 설정
+  // 끝에 슬래시 포함 여부 확인하고 http 설정
   url = url.includes('http') ? url : `https://${url}`;
   return url.endsWith('/') ? url : `${url}/`;
 }
